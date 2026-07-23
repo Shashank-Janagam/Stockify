5cd145d552c78090eb417917e4907b08f5002762
Shashank-Janagam <shashijanagam2004@gmail.com>
2026-07-23 21:16:57 +0530

    using upstox1

--- Stockify-Frontend/src/components/portfolio/AlgoDashboard.tsx
+++ Stockify-Frontend/src/components/portfolio/AlgoDashboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback, useRef } from "react";
+import { useEffect, useState, useCallback } from "react";
 import "../../Styles/AlgoDashboard.css";
 
 type AlgoConfig = {
